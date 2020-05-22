fcd261e8e7d9b82615c067b8f969b8704e0e8268
Your Name <you@example.com>
2020-05-21 21:26:17 -0600

    more db setup. added controller folder and files, a little server.js modification

--- server.js
+++ server.js
@@ -29,8 +29,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./app/routing/apiRoutes.js')(app);
-require('./app/routing/htmlRoutes.js')(app);
+//require('./app/routing/apiRoutes.js')(app);
+//require('./app/routing/htmlRoutes.js')(app);
 
 app.listen(PORT, function() {
   console.log("App is listening on PORT " + PORT);
